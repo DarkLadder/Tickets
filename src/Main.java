fe1bfba6d05fe247c696c5ff124ed501199cfdbb
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-01-29 23:57:06 +0300

    v2.0

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,18 @@
  */
 public class Main {
     public static void main(String[] args) {
-
-        for (int i=0; i <= 999999; i++) {
-            int gh = 0;
+        int count = 0;
+        for (int i=000000; i <= 999999; i++) {
+            int gh = i;
             int a, b, c, d, e, f;
             a = gh/100000;
-            b = gh/10000;
-            b = b%10;
-            c = gh/1000;
-            c = c%10;
-            d = gh/100;
-            d = d%10;
-            e = gh/10;
-            e = e%10;
-            f = gh/1;
-            f = f%10;
-            Calc calc = new Calc(a, b, c, d, e, f);
-            calc.Calcul();
+            b = gh/10000%10;
+            c = gh/1000%10;
+            d = gh/10%100;
+            e = gh/10%10;
+            f = gh/1%10;
+            if (a + b + c == d + e + f) count++;
         }
+        System.out.println(count);
     }
 }
