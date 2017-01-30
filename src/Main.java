c897c83a29f67fa71985900a2545fbc80360a8c8
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-01-30 20:15:48 +0300

    Final release
    This program has successfully solved the task

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 /**
- * Created by BYT9C0D9 on 07.12.2016.
+ * Created by Dmitrii on 07.12.2016.
  */
 public class Main {
     public static void main(String[] args) {
         int count = 0;
-        for (int i=000000; i <= 999999; i++) {
+        for (int i=0; i <= 999999; i++) {
             int gh = i;
             int a, b, c, d, e, f;
             a = gh/100000;
             b = gh/10000%10;
             c = gh/1000%10;
-            d = gh/10%100;
+            d = gh/100%10;
             e = gh/10%10;
             f = gh/1%10;
-            if (a + b + c == d + e + f) count++;
+            int x = a + b + c;
+            int y = d + e + f;
+            if (x == y) {count++;}
         }
         System.out.println(count);
     }
